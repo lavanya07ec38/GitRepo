da63b370dfc5e1d98feb55ec02c7656b4072c79f
lavanya.07ec38 <lavanya.07ec38@gmail.com>
2020-04-21 14:49:23 +0530

    initial commit

---
+++ src/main/java/com/qa/bases/TestBase.java
@@ -0,0 +1,13 @@
+package com.qa.bases;
+
+import org.testng.annotations.Test;
+
+public class TestBase {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("Test Base-Test1 method");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/HomeTest.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class HomeTest {
+	
+	@Test
+	public void homeTest() {
+		
+		System.out.println("Home test method");
+	}
+
+}
